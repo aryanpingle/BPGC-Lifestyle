2c2d039e1d46b04fb14482daebe500d673ad4957
robo-mop <realaryanpingle@gmail.com>
2022-11-12 21:12:34 +0530

    More changes

--- driver/SafeInput.java
+++ driver/SafeInput.java
@@ -18,6 +18,12 @@ public class SafeInput {
             System.exit(0);
         }
     }
+
+    /**
+     * Returns the user input as a String
+     * @return The user input
+     * @throws BackException
+     */
     
     public static final String inputLine() throws BackException {
         String input = sc.nextLine();
@@ -26,14 +32,30 @@ public class SafeInput {
 
         return input;
     }
+
+    /**
+     * Returns the user input as an int
+     * @return The user input
+     * @throws BackException
+     */
     
     public static final int inputInteger() throws BackException {
         return Integer.parseInt(inputLine());
     }
+
+    /**
+     * Returns the user input as a double
+     * @return The user input
+     * @throws BackException
+     */
     
     public static final double inputDouble() throws BackException {
         return Double.parseDouble(inputLine());
     }
+    
+    /**
+     * Pauses execution until the user enters some value
+     */
 
     public static final void waitForInput() {
         sc.nextLine();
