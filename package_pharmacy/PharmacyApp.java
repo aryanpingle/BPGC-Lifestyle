fc2cb956e33801cdfab08c67f1e0ddec047ba6c4
robo-mop <realaryanpingle@gmail.com>
2022-12-13 21:23:57 +0530

    Extend functionality

---
+++ package_pharmacy/PharmacyApp.java
@@ -0,0 +1,14 @@
+package package_pharmacy;
+
+import driver.App;
+
+public class PharmacyApp extends App {
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public String getAppName() {
+        return "Pharmacy API";
+    }
+}
